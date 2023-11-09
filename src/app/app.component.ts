151b46e74e797117a98082fca509f572c3021241
Farhad rasouli <rasouli.farhad@gmail.com>
2023-11-10 01:26:26 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './heroes/shared/hero';
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'name 11'},
+  {id: 12, name: 'name 12'},
+  {id: 13, name: 'name 13'},
+  {id: 14, name: 'name 14'},
+  {id: 15, name: 'name 15'},
+  {id: 16, name: 'name 16'},
+  {id: 17, name: 'name 17'},
+  {id: 18, name: 'name 18'},
+  {id: 19, name: 'name 19'},
+  {id: 20, name: 'name 20'},
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'masterDetailApp';
+  title = 'Tour of Heroes';
+
+  heroes = HEROES;
+
+  selectedHero!: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
